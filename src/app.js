8b01b85454377ae482a613467dcde6e361554044
jayesh narola <aadhyainfotech02@gmail.com>
2024-11-20 17:57:21 +0530

    no message

--- src/app.js
+++ src/app.js
@@ -36,7 +36,7 @@ app.use(
 );
 
 // app.use(express.static(path.join(__dirname, "public")));
-app.use(express.static("public"));
+app.use(express.static("./public"));
 app.use("/api", routes);
 
 app.use((err, req, res, next) => {
@@ -57,5 +57,3 @@ app.get("/", (req, res) =>
 app.listen(port, function () {
   console.log(`Express server listening on port ${port}`);
 });
-
-
